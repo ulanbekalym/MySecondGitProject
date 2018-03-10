562d0f5e1e8b8d555386501719bccb8d01e5ea0d
ulanbekalym <ulanbekalym@10.1.10.151>
2018-03-10 11:22:43 -0600

    version 2

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -1,5 +1,10 @@
 package main;
 
 public class GoodEvening {
-
+	public static void main(String[] args) {
+		System.out.println("Good Evening Class!");
+		
+		System.out.println("My name is Maria");
+	}
+    
 }
